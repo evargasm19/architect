05c7441d0465603f53fe0d40c252caa3ed319f42
evargasm19 <evargasm19@gmail.com>
2023-04-17 09:25:33 -0500

    Lines added to Men2.java

--- src/com/flipkart/men/Men2.java
+++ src/com/flipkart/men/Men2.java
@@ -1,5 +1,16 @@
 package com.flipkart.men;
 
 public class Men2 {
+	
+	public void hello() {
+		System.out.println("hello");
+		System.out.println("Git learning");
+		
+		System.out.println("Line added at the same time, here by user git evargasm19");
+		System.out.println("Line 2 added at the same time, here by user git evargasm19");
+		
+		System.out.println("Line added at the same time, here by user git evargasm19 HARD WORK = JOB");
+
+	}
 
 }

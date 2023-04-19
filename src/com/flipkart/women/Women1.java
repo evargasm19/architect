3aef8c615ffd9ed9fdd346ebfb66ebc5c1f96a29
evar5594 <evar5594@elpoli.edu.co>
2023-04-19 01:56:02 -0500

    Women1.java

--- src/com/flipkart/women/Women1.java
+++ src/com/flipkart/women/Women1.java
@@ -2,7 +2,8 @@ package com.flipkart.women;
 
 public class Women1 {
 	public void diagram() {
-		System.out.println("cloner to architec");
+		System.out.println("cloner to architec"
+				+ "");
 
 	}
 

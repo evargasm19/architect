f03046b27cd1f1be21a738acddda2ba9ae8e87f2
evargasm19 <evargasm19@gmail.com>
2023-04-19 06:04:38 -0500

    understanding

--- src/com/flipkart/kids/Kid3.java
+++ src/com/flipkart/kids/Kid3.java
@@ -3,14 +3,15 @@ package com.flipkart.kids;
 public class Kid3 {
 	
 
-	public void seleniumJava() {
+	public void selenium_Architec() {
 		System.out.println("line written by the architec");
 
 	}
 
 
 
-	public void seleniumJava() {
+	public void selenium_cloner() 
+	{
 		System.out.println("line written by the cloner");
 
 	}
